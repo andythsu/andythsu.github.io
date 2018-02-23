481d36529d41226d84390e5440a76d848e6b3c8c
Su <Andy.Su@allstream.com>
2018-02-23 16:15:42 -0500

    deleted unnecessary files

--- website/details/js/ng/service/skills_collector.js
+++
@@ -1,9 +0,0 @@
-myApp.service('allSkills', [function(){
-  var skills_array = [];
-  this.add = function(skill){
-    array.push(skill);
-  }
-  this.getArray = function(){
-    return skills_array;
-  }
-}]);
