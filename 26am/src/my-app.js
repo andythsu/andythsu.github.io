31ca3fff51728fab7730b46d97fc0f6d29b2cbc0
Andy Su <andy.su@dematic.com>
2018-12-12 17:22:45 -0500

    used Litelement instead of PolymerElement

--- 26am/src/my-app.js
+++ 26am/src/my-app.js
@@ -1,18 +1,22 @@
-import {PolymerElement, html} from 'https://unpkg.com/@polymer/polymer/polymer-element.js?module';
 import 'https://unpkg.com/@polymer/paper-button/paper-button.js?module';
+import {LitElement, html} from 'https://unpkg.com/@polymer/lit-element@0.6.4/lit-element.js?module';
+class MyApp extends LitElement {
 
-class MyApp extends PolymerElement {
+  constructor(){
+    super();
+  }
+
   static get is(){ return 'my-app'; }
 
-  static get template() {
+  render(){
     return html`
-      <h1>Success!</h1>
-        <paper-button class="pink">link</paper-button>
-        <paper-button>Flat button</paper-button>
-        <paper-button raised>Raised button</paper-button>
-        <paper-button noink>No ripple effect</paper-button>
-        <paper-button toggles>Toggle-able button</paper-button>
-    `;
+       <h1>Success!</h1>
+         <paper-button class="pink">link</paper-button>
+         <paper-button>Flat button</paper-button>
+         <paper-button raised>Raised button</paper-button>
+         <paper-button noink>No ripple effect</paper-button>
+         <paper-button toggles>Toggle-able button</paper-button>
+     `; 
   }
 
   static get properties() {
