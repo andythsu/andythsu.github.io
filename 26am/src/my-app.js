7c37a4cda22b92454f1ae75d942a4f04317fa2ca
Andy Su <andy.su@dematic.com>
2018-12-12 17:17:22 -0500

    tested with buttons

--- 26am/src/my-app.js
+++ 26am/src/my-app.js
@@ -1,11 +1,17 @@
 import {PolymerElement, html} from 'https://unpkg.com/@polymer/polymer/polymer-element.js?module';
+import 'https://unpkg.com/@polymer/paper-button/paper-button.js?module';
 
 class MyApp extends PolymerElement {
   static get is(){ return 'my-app'; }
 
   static get template() {
     return html`
-      <h1>Success!</h1>     
+      <h1>Success!</h1>
+        <paper-button class="pink">link</paper-button>
+        <paper-button>Flat button</paper-button>
+        <paper-button raised>Raised button</paper-button>
+        <paper-button noink>No ripple effect</paper-button>
+        <paper-button toggles>Toggle-able button</paper-button>
     `;
   }
 
