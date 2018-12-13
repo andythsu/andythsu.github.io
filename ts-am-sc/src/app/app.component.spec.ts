52f049a5088efb711ca234df36df0a56e228db53
Andy Su <andy.su@dematic.com>
2018-12-13 10:07:03 -0500

    Cleaned code

--- ts-am-sc/src/app/app.component.spec.ts
+++ ts-am-sc/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'heroes'`, () => {
+  it(`should have as title 'ts-am-sc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('heroes');
+    expect(app.title).toEqual('ts-am-sc');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to heroes!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ts-am-sc!');
   });
 });
