800d382b1c5864e071b030357be60518affd0c38
Andy Su <f2280c@gmail.com>
2018-12-21 17:29:36 -0500

    added list scroll bar

--- ts-am-sc/src/app/list/list.component.ts
+++ ts-am-sc/src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ListService } from './list.service';
 
 @Component({
@@ -6,8 +6,55 @@ import { ListService } from './list.service';
   providers: [ListService],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
+  // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ListComponent implements OnInit {
+
+  folders = [
+    {
+      "name" : "folder 1",
+      "date" : "2018-01-01"
+    },
+    {
+      "name" : "folder 2",
+      "date" : "2018-02-02"
+    },
+    {
+      "name": "folder 3",
+      "date": "2018-03-20"
+    },
+    {
+      "name": "folder 4",
+      "date": "2018-04-21"
+    },
+    {
+      "name": "folder 5",
+      "date": "2018-05-30"
+    }
+  ];
+
+  notes = [
+    {
+      "name" : "note 1",
+      "date" : "2018-03-02"
+    },
+    {
+      "name" : "note 2",
+      "date" : "2018-05-10"
+    },
+    {
+      "name": "note 3",
+      "date": "2018-11-30"
+    },
+    {
+      "name": "note 4",
+      "date": "2018-12-21"
+    },
+    {
+      "name": "note 5",
+      "date": "2018-12-30"
+    }
+  ];
 
   constructor() { }
 
